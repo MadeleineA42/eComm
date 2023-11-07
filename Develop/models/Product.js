4f02a34c7463b3e2cb3f37b86b5c0342eec05e78
MadeleineA42 <abraham.madeleine229@gmail.com>
2023-11-07 13:36:28 -0500

    filled out models files

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -9,7 +9,39 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER, //sets the datatype to an integer
+      allowNull: false, //disallows null values
+      primaryKey: true, //sets this as the primary key
+      autoIncrement: true, 
+    },
+    product_name: {
+      type: DataTypes.STRING, //sets the datatype to a string
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL(10, 2), //sets the datatype to a decimal value allowing decimal places
+      allowNull: false,
+      validate: { //validation that the value is a decimal
+        isDecimal: true,
+      },
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10, //sets the default value to 10
+      validate: { //validates the value as a number
+        isNumeric: true,
+      },
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      references: { //will refer to the "category table"
+        model: 'category',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
--- server.js
+++ server.js
@@ -18,3 +18,4 @@ app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
 
+
